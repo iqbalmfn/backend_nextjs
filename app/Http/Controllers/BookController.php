c1b191fa2e08c3b3272a3954e2894ed7b4bbc6dc
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2022-09-19 21:05:09 +0700

    form handle

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,39 +5,52 @@ namespace App\Http\Controllers;
 use App\Http\Resources\BookResource;
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BookController extends Controller
 {
-    public function index()
-    {
-        $books = Book::all();
-        $data = BookResource::collection($books);
+  public function index()
+  {
+    $books = Book::orderByDesc('id')->get();
+    $data = BookResource::collection($books);
 
-        return $this->sendResponse($data, "Successfully get books");
+    return $this->sendResponse($data, "Successfully get books");
+  }
+
+  public function store(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      "name"  => "required|min:4",
+      "description" => "required|min:10|max:300",
+      "price" => "required"
+    ]);
+
+    if ($validator->fails()) {
+      return $this->sendError("Validation error", $validator->errors());
     }
 
-    public function store(Request $request)
-    {
-        //
-    }
+    $store = Book::create($request->all());
 
-    public function show(Book $book)
-    {
-        //
-    }
+    return $this->sendResponse($store, "Successfully add books");
+  }
 
-    public function edit(Book $book)
-    {
-        //
-    }
+  public function show(Book $book)
+  {
+    //
+  }
 
-    public function update(Request $request, Book $book)
-    {
-        //
-    }
+  public function edit(Book $book)
+  {
+    //
+  }
 
-    public function destroy(Book $book)
-    {
-        //
-    }
+  public function update(Request $request, Book $book)
+  {
+    //
+  }
+
+  public function destroy(Book $book)
+  {
+    //
+  }
 }
