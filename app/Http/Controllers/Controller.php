eccfeeecdd837288929605bd19e9ae9692f83951
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2022-09-19 13:21:56 +0700

    format response API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,27 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function sendResponse($result, $message) {
+        $response = [
+            "success" => true,
+            "message" => $message,
+            "data"    => $result
+        ];
+
+        return response()->json($response, 200);
+    } 
+
+    public function sendError($error, $errorMessage = [], $code = 404) {
+        $response = [
+            "success" => false,
+            "message" => $error,
+        ];
+
+        if (!empty($errorMessage)) {
+            $response['data'] = $errorMessage;
+        }
+
+        return response()->json($response, $code);
+    } 
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
   Route::get('/user', function (Request $request) {
     return $request->user();
   });
-  Route::resource('books', BookController::class);
 });
+
+Route::resource('books', BookController::class);
