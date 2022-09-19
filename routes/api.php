421cdac738346c16b5f279266f2f5b7528d4b423
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2022-09-19 18:48:08 +0700

    .

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
   Route::get('/user', function (Request $request) {
     return $request->user();
   });
-  Route::resource('books', BookController::class);
 });
+Route::resource('books', BookController::class);
